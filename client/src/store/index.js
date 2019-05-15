a1171c0d6da8a002624841de0ed35087089f3e84
Markus Hakala <markus.hakala@kolumbus.fi>
2019-05-15 12:34:19 +0300

    changes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,18 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import rootReducer from '../reducers/root-reducer';
+import { createStore, applyMiddleware, compose  } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
 const initialState = {};
+
 const middleware = [thunk];
 
-const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+const store = createStore(
+    rootReducer, 
+    initialState, 
+    compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    ));
 
-const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
-
-const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
-
-console.log(store.getState());
 
 export default store;
